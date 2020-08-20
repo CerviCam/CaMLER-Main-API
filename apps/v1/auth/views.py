523dad34d50a648d6942877e5d8c54d6bc29abc3
Nandhika Prayoga <nann011530@gmail.com>
2020-08-20 20:47:19 +0700

    Send 401 status code for failed login

--- apps/v1/auth/views.py
+++ apps/v1/auth/views.py
@@ -1,7 +1,7 @@
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-
+from rest_framework import status
 # Create your views here.
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
@@ -15,6 +15,6 @@ class CustomAuthToken(ObtainAuthToken):
 
             return Response({
                 'token': token.key,
-            })
+            }, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors) 
+            return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED) 
